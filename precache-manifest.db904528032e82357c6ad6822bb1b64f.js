0ff38602f201235d23e0d3db06858cbe4664e516
JoshuaKiplimo <joshuakiprono8@gmail.com>
2022-10-02 20:43:48 -0400

    Updates

---
+++ precache-manifest.db904528032e82357c6ad6822bb1b64f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339d52ae41956457d4620cb2ca19085a",
+    "url": "/onlineShoppingCart/index.html"
+  },
+  {
+    "revision": "142ff93e67cdcdae958d",
+    "url": "/onlineShoppingCart/static/css/main.35134b96.chunk.css"
+  },
+  {
+    "revision": "e7588b0ee59d611193ef",
+    "url": "/onlineShoppingCart/static/js/2.ed965204.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/onlineShoppingCart/static/js/2.ed965204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142ff93e67cdcdae958d",
+    "url": "/onlineShoppingCart/static/js/main.5c470709.chunk.js"
+  },
+  {
+    "revision": "170a1c86d0bec8e58301",
+    "url": "/onlineShoppingCart/static/js/runtime-main.4e19b81d.js"
+  }
+]);
